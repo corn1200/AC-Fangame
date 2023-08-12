4bf7073518194e3ee3b84bb2f491f1ab52c8444c
김현승 <corn120033@gmail.com>
2023-08-12 23:32:55 +0900

    플레이어 점프 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +12,9 @@ public class Player : MonoBehaviour
 
     private Vector3 dir = Vector3.zero;
 
+    private bool ground = false;
+    public LayerMask layer;
+
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody>();
@@ -22,6 +25,13 @@ public class Player : MonoBehaviour
         dir.x = Input.GetAxis("Horizontal");
         dir.z = Input.GetAxis("Vertical");
         dir.Normalize();
+
+        CheckGround();
+        if (Input.GetButtonDown("Jump") && ground)
+        {
+            Vector3 jumpPower = Vector3.up * jumpHeight;
+            rigidbody.AddForce(jumpPower, ForceMode.VelocityChange);
+        }
     }
 
     private void FixedUpdate()
@@ -29,7 +39,7 @@ public class Player : MonoBehaviour
         if (dir != Vector3.zero)
         {
             // 지금 바라보는 방향의 부호 != 나아갈 방향 부호
-            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x) 
+            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x)
                 || Mathf.Sign(transform.forward.z) != Mathf.Sign(dir.z))
             {
                 transform.Rotate(0, 1, 0);
@@ -38,5 +48,20 @@ public class Player : MonoBehaviour
         }
 
         rigidbody.MovePosition(this.gameObject.transform.position + dir * speed * Time.deltaTime);
+    }
+
+    void CheckGround()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.2f),
+            Vector3.down, out hit, 0.4f, layer))
+        {
+            ground = true;
+        }
+        else
+        {
+            ground = false;
+        }
     }
 }
