4b9b04efc4cee37f1aa65acef507363d63e76aff
김현승 <corn120033@gmail.com>
2023-09-23 23:35:14 +0900

    플레이어 방향 전화 코드 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rigidbody;
-    public float speed = 10;
-    public float jumpHeight = 3;
-    public float dash = 5;
+    public Vector3 dir = Vector3.zero;
     public float rotSpeed = 3;
-
-    private Vector3 dir = Vector3.zero;
-
-    private bool ground = false;
-    public LayerMask layer;
 
     void Start()
     {
@@ -26,48 +20,14 @@ public class Player : MonoBehaviour
         dir.z = Input.GetAxis("Vertical");
         dir.Normalize();
 
-        CheckGround();
-        if (Input.GetButtonDown("Jump") && ground)
-        {
-            Vector3 jumpPower = Vector3.up * jumpHeight;
-            rigidbody.AddForce(jumpPower, ForceMode.VelocityChange);
-        }
-
-        if(Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashPower = transform.forward * -Mathf.Log(1/rigidbody.drag) * dash;
-            rigidbody.AddForce(dashPower, ForceMode.VelocityChange);
-        }
+        Debug.Log(dir.ToString());
     }
 
     private void FixedUpdate()
     {
         if (dir != Vector3.zero)
         {
-            // 지금 바라보는 방향의 부호 != 나아갈 방향 부호
-            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x)
-                || Mathf.Sign(transform.forward.z) != Mathf.Sign(dir.z))
-            {
-                transform.Rotate(0, 1, 0);
-            }
             transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed * Time.deltaTime);
-        }
-
-        rigidbody.MovePosition(this.gameObject.transform.position + dir * speed * Time.deltaTime);
-    }
-
-    void CheckGround()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.2f),
-            Vector3.down, out hit, 0.4f, layer))
-        {
-            ground = true;
-        }
-        else
-        {
-            ground = false;
         }
     }
 }
