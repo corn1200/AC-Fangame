f65808cc5c2eb6c34c5f02a38f6f5de07089dbcd
김현승 <corn120033@gmail.com>
2023-09-25 21:54:45 +0900

    회전 방식 변경

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,31 +6,61 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rigidbody;
-    public Vector3 dir = Vector3.zero;
-    public float rotSpeed = 40;
-    public float moveSpeed = 4;
+    public Vector3 startDir;
+    public Vector3 targetDir = Vector3.zero;
+    public float rotTime = 0.1f;
+    public float currentTime = 0;
+    public float moveSpeed = 20;
+    public bool isRotate = false;
 
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody>();
+        startDir = this.transform.forward;
     }
 
     void Update()
     {
-        dir.x = Input.GetAxis("Horizontal");
-        dir.z = Input.GetAxis("Vertical");
-        dir.Normalize();
+        if (!isRotate)
+        {
+            targetDir.x = Input.GetAxis("Horizontal");
+            targetDir.z = Input.GetAxis("Vertical");
+            targetDir.Normalize();
 
-        Debug.Log(dir.ToString());
+            Debug.Log(targetDir.ToString());
+
+            if (targetDir != Vector3.zero)
+            {
+                isRotate = true;
+            }
+        }
+
+        if (isRotate)
+        {
+            currentTime += Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
     {
-        if (dir != Vector3.zero)
+        if (isRotate)
         {
-            transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed * Time.deltaTime);
+            if (currentTime >= rotTime)
+            {
+                currentTime = rotTime;
+            }
+
+            float t = currentTime / rotTime;
+            transform.forward = Vector3.Slerp(startDir, targetDir, t);
+
+            if (currentTime >= rotTime)
+            {
+                isRotate = false;
+                startDir = transform.forward;
+                currentTime = 0;
+            }
         }
-        rigidbody.MovePosition(this.gameObject.transform.position + 
-            dir * moveSpeed * Time.deltaTime);
+        rigidbody.MovePosition(this.gameObject.transform.position +
+            targetDir * moveSpeed * Time.deltaTime);
     }
 }
