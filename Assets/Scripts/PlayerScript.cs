c5ba5bd7b4e7eef1bedf2c36037fbf091ded58e6
김현승 <corn120033@gmail.com>
2023-11-15 23:30:57 +0900

    모든 키 맵핑 & 이벤트 추가

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,7 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody rigidbody;
 
     private Vector3 moveDirection;
-    public float moveSpeed = 500f;
+    public float moveSpeed = 100f;
 
     void Start()
     {
@@ -28,9 +28,83 @@ public class PlayerScript : MonoBehaviour
         moveDirection = new Vector3(input.x, 0f, input.y);
     }
 
-    private void OnMouseDown()
+    public void OnBoost(InputAction.CallbackContext context)
     {
-        Debug.Log("shangus1");
-        rigidbody.AddForce(transform.forward * moveSpeed);
+        Debug.Log("OnBoost : " + context.ReadValueAsButton());
+    }
+    
+    public void OnQuickBoost(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnQuickBoost : " + context.ReadValueAsButton());
+    }
+    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnJump : " + context.ReadValueAsButton());
+    }
+    
+    public void OnAssaultBoost(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnAssaultBoost : " + context.ReadValueAsButton());
+    }
+    
+    public void OnTargetAssist(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnTargetAssist : " + context.ReadValueAsButton());
+    }
+    
+    public void OnLeftWeapon(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnLeftWeapon : " + context.ReadValueAsButton());
+    }
+    
+    public void OnRightWeapon(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnRightWeapon : " + context.ReadValueAsButton());
+    }
+    
+    public void OnLeftShoulderWeapon(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnLeftShoulderWeapon : " + context.ReadValueAsButton());
+    }
+    
+    public void OnRightShoulderWeapon(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnRightShoulderWeapon : " + context.ReadValueAsButton());
+    }
+    
+    public void OnShiftControl(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnShiftControl : " + context.ReadValueAsButton());
+    }
+    
+    public void OnRepairKit(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnRepairKit : " + context.ReadValueAsButton());
+    }
+    
+    public void OnAccess(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnAccess : " + context.ReadValueAsButton());
+    }
+    
+    public void OnScan(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnScan : " + context.ReadValueAsButton());
+    }
+    
+    public void OnWeaponDisarm(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnWeaponDisarm : " + context.ReadValueAsButton());
+    }
+    
+    public void OnChoiceLeft(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnChoiceLeft : " + context.ReadValueAsButton());
+    }
+    
+    public void OnChoiceRight(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnChoiceRight : " + context.ReadValueAsButton());
     }
 }
