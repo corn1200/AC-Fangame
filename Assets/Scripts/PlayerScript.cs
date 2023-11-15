3064eb016a2be465c382a16e6be8353a7604e71e
김현승 <corn120033@gmail.com>
2023-11-15 22:11:18 +0900

    이동 기능 AddForce 방식으로 수정

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Build;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody rigidbody;
+
     private Vector3 moveDirection;
-    public float moveSpeed = 4f;
+    public float moveSpeed = 500f;
 
     void Start()
     {
@@ -19,24 +18,19 @@ public class PlayerScript : MonoBehaviour
     {
         if (moveDirection != Vector3.zero)
         {
-            Debug.Log(Vector3.Normalize(moveDirection));
-            Debug.Log(moveDirection.normalized);
-            rigidbody.AddForce(Vector3.Normalize(moveDirection) * Time.fixedDeltaTime 
-                * moveSpeed, ForceMode.VelocityChange);
-            if(rigidbody.velocity.magnitude > 5)
-            {
-                rigidbody.velocity = rigidbody.velocity.normalized * 5;
-            }
+            rigidbody.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
         }
-        Debug.Log(rigidbody.velocity);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
-        if (input != null)
-        {
-            moveDirection = new Vector3(input.x, 0f, input.y);
-        }
+        moveDirection = new Vector3(input.x, 0f, input.y);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("shangus1");
+        rigidbody.AddForce(transform.forward * moveSpeed);
     }
 }
