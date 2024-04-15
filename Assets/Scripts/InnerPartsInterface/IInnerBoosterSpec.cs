6a76f9417789f12604a290f436a19b04b5d53d1c
김현승 <corn120033@gmail.com>
2024-04-15 13:00:28 +0900

    내장 부스터 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/InnerPartsInterface/IInnerBoosterSpec.cs
@@ -0,0 +1,20 @@
+﻿namespace InnerPartsInterface
+{
+    public interface IInnerBoosterSpec
+    {
+        public float Thrust { get; set; }
+        public float UpwardThrust { get; set; }
+        public float UpwardConsumptionEnergy { get; set; }
+        public float QuickBoostThrust { get; set; }
+        public float QuickBoostSprayTime { get; set; }
+        public float QuickBoostConsumptionEnergy { get; set; }
+        public float QuickBoostCooldownTime { get; set; }
+        public float QuickBoostReusableGuaranteedWeight { get; set; }
+        public float AssaultBoostThrust { get; set; }
+        public float AssaultBoostConsumptionEnergy { get; set; }
+        public float MeleeAttackThrust { get; set; }
+        public float MeleeAttackConsumptionEnergy { get; set; }
+        public float Weight { get; set; }
+        public float EnergyLoad { get; set; }
+    }
+}
