7e1133fdfae1c80c5460e9edbdd9395706a26f4a
김현승 <corn120033@gmail.com>
2024-04-15 12:36:06 +0900

    프레임 팔 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameArmSpec.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameArmSpec
+    {
+        public float ArmsLoadLimit { get; set; }
+        public float RecoilControl { get; set; }
+        public float FirearmSpecialization { get; set; }
+        public float MeleeSpecialization { get; set; }
+    }
+}
