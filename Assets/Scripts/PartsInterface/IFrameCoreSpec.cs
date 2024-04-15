c909b9429b3ea3cc7c028631cac07ce9a7a6a771
김현승 <corn120033@gmail.com>
2024-04-15 12:33:58 +0900

    프레임 코어 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameCoreSpec.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameCoreSpec
+    {
+        public float AttitudeStability { get; set; }
+        public float BoosterEfficiencyAdj { get; set; }
+        public float GeneratorOutputAdj { get; set; }
+        public float GeneratorSupplyAdj { get; set; }
+    }
+}
