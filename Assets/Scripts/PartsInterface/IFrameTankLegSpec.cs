c1813c6c4c1b9111ce1da3d7d7aeca68b5f188a2
김현승 <corn120033@gmail.com>
2024-04-15 12:46:47 +0900

    프레임 다리(탱크) 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameTankLegSpec.cs
@@ -0,0 +1,20 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameTankLegSpec
+    {
+        public float AttitudeStability { get; set; }
+        public float LoadLimit { get; set; }
+        public float DrivingPerformance { get; set; }
+        public float HighSpeedDrivingPerformance { get; set; }
+        public float DrivingForce { get; set; }
+        public float UpwardMomentum { get; set; }
+        public float RisingConsumptionEnergy { get; set; }
+        public float QuickBoostForce { get; set; }
+        public float QuickBoostSprayTime { get; set; }
+        public float QuickBoostConsumptionEnergy { get; set; }
+        public float QuickBoostCooldownTime { get; set; }
+        public float QuickBoostReusableGuaranteedWeight { get; set; }
+        public float AssaultBoostForce { get; set; }
+        public float AssaultBoostConsumptionEnergy { get; set; }
+    }
+}
