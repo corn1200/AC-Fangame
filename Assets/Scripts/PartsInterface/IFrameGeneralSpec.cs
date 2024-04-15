5911e91ea6ddcb694e76926ddaac90a7678d0af5
김현승 <corn120033@gmail.com>
2024-04-15 12:24:23 +0900

    프레임 공통 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameGeneralSpec.cs
@@ -0,0 +1,12 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameGeneralSpec
+    {
+        public float ArmorPoint { get; set; }
+        public float Weight { get; set; }
+        public float EnergyLoad { get; set; }
+        public float AntiKineticDefence { get; set; }
+        public float AntiEnergyDefence { get; set; }
+        public float AntiExplosiveDefence { get; set; }
+    }
+}
