0c163cdd06f2e6722f81688cceda80c2f73b88e1
김현승 <corn120033@gmail.com>
2024-04-15 12:39:04 +0900

    프레임 다리 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameLegSpec.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameLegSpec
+    {
+        public float AttitudeStability { get; set; }
+        public float LoadLimit { get; set; }
+        public float JumpDistance { get; set; }
+        public float JumpHeight { get; set; }
+    }
+}
