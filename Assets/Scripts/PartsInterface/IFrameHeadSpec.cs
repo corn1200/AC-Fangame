58ce093e81b82da48b9b667d50dd41fc963b12d0
김현승 <corn120033@gmail.com>
2024-04-15 12:31:17 +0900

    프레임 머리 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/PartsInterface/IFrameHeadSpec.cs
@@ -0,0 +1,11 @@
+﻿namespace PartsInterface
+{
+    public interface IFrameHeadSpec
+    {
+        public float AttitudeStability { get; set; }
+        public float SystemRecovery { get; set; }
+        public float ScanDistance { get; set; }
+        public float ScanEffectDuration { get; set; }
+        public float ScanWaitingTime { get; set; }
+    }
+}
