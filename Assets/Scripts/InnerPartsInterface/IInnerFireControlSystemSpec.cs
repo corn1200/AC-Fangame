c3a99cbccffad85abe76df751c8ac98a87601fa8
김현승 <corn120033@gmail.com>
2024-04-16 13:46:23 +0900

    내장 FCS 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/InnerPartsInterface/IInnerFireControlSystemSpec.cs
@@ -0,0 +1,13 @@
+﻿namespace InnerPartsInterface
+{
+    public interface IInnerFireControlSystemSpec
+    {
+        public float ShortDistanceAssist { get; set; }
+        public float MiddleDistanceAssist { get; set; }
+        public float LongDistanceAssist { get; set; }
+        public float MissileAimAdj { get; set; }
+        public float MultiAimAdj { get; set; }
+        public float Weight { get; set; }
+        public float EnergyLoad { get; set; }
+    }
+}
