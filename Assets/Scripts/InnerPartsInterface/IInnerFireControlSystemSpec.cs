df00ed623165d15916a02ea29371e5c69f4d878c
김현승 <corn120033@gmail.com>
2024-04-16 14:00:20 +0900

    내장 제너레이터 파츠 스펙 인터페이스 작성

--- Assets/Scripts/InnerPartsInterface/IInnerFireControlSystemSpec.cs
+++ Assets/Scripts/InnerPartsInterface/IInnerFireControlSystemSpec.cs
@@ -2,9 +2,9 @@
 {
     public interface IInnerFireControlSystemSpec
     {
-        public float ShortDistanceAssist { get; set; }
-        public float MiddleDistanceAssist { get; set; }
-        public float LongDistanceAssist { get; set; }
+        public float ShortDistanceAssistSpecialization { get; set; }
+        public float MiddleDistanceAssistSpecialization { get; set; }
+        public float LongDistanceAssistSpecialization { get; set; }
         public float MissileAimAdj { get; set; }
         public float MultiAimAdj { get; set; }
         public float Weight { get; set; }
---
+++ Assets/Scripts/InnerPartsInterface/IInnerGeneratorSpec.cs
@@ -0,0 +1,13 @@
+﻿namespace InnerPartsInterface
+{
+    public interface IInnerGeneratorSpec
+    {
+        public float EnergyCapacity { get; set; }
+        public float EnergyChargingPerformance { get; set; }
+        public float SupplyRestorationPerformance { get; set; }
+        public float ChargingEnergyUponRestoration { get; set; }
+        public float EnergyFirearmSpecialization { get; set; }
+        public float Weight { get; set; }
+        public float EnergyOutput { get; set; }
+    }
+}
