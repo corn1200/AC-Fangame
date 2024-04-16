9fc2b5fc8d0c9802bc5e91077368e864ef7ffc3f
김현승 <corn120033@gmail.com>
2024-04-16 14:06:15 +0900

    내장 어설트 아머 파츠 스펙 인터페이스 작성

---
+++ Assets/Scripts/InnerPartsInterface/IInnerAssaultArmorExpansion.cs
@@ -0,0 +1,12 @@
+﻿namespace InnerPartsInterface
+{
+    public interface IInnerAssaultArmorExpansion
+    {
+        public float AttackPower { get; set; }
+        public float ImpactPower { get; set; }
+        public float ImpactResidual { get; set; }
+        public float ExplosionRange { get; set; }
+        public float EffectArea { get; set; }
+        public float DirectHitCompensation { get; set; }
+    }
+}
