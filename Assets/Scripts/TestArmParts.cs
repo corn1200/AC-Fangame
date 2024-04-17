1872f8fdfdff3cad8aaf98861fc33f7898d6d12f
김현승 <corn120033@gmail.com>
2024-04-17 12:57:51 +0900

    프로젝트 정리

---
+++ Assets/Scripts/TestArmParts.cs
@@ -0,0 +1,15 @@
+﻿using FramePartsInterface;
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [Serializable]
+    public class TestArmParts : MonoBehaviour, IFrameArmSpec
+    {
+        public float ArmsLoadLimit { get; set; }
+        public float RecoilControl { get; set; }
+        public float FirearmSpecialization { get; set; }
+        public float MeleeSpecialization { get; set; }
+    }
+}
